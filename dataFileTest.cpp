e10882f71d9bcf6e2ae84798968214394538619f
piz-zawitch <buciorexelreynald@gmail.com>
2025-10-31 20:13:05 +0800

    fidxed

--- dataFileTest.cpp
+++ dataFileTest.cpp
@@ -5,14 +5,15 @@ using namespace std;
 
 int main()
 {
-    string line = "", first = "", id = "";
-    int location = 0;
+    string line = "", first = "", id = "", w[5] = {"","","","","",};
+    int location = 0, accID = 0, accColumn[accID] = {};
 
     ifstream accountFile;
     accountFile.open("accounts.csv");
-
     getline(accountFile, line);
-    getline(accountFile, line);
+    
+    //process loading
+    while( getline(accountFile, line)){
 
     location = line.find(",");
     first = line.substr(0, location);
@@ -21,9 +22,25 @@ int main()
     location = line.find(",");
     id = line.substr(0, location);
     line = line.substr(location + 1, line.length());
+    
+    for(int i = 0;i<5;i++)
+    {
+    location = line.find(",");
+    w[i] = line.substr(0, location);
+    line = line.substr(location + 1, line.length());
+    };
+    
+    cout << first << endl;
+    cout << id << endl;
+    for(int i = 0;i<5;i++)
+    {
+    cout << w[i];
+    }
+    cout << endl << endl;
+        }
 
+    
 
-    cout << id;
     
 
     cin.ignore();
